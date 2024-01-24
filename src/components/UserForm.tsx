e35446a8e7875ed58103af9c7afddd216e0d38cc
Robin Singh <44robin.rs@gmail.com>
2024-01-24 18:19:08 +0530

    build error fixed

--- src/components/UserForm.tsx
+++ src/components/UserForm.tsx
@@ -80,7 +80,7 @@ const UserForm = ({
           <CiCircleInfo />
         </span>
         <span className="text-xs">
-          Please note, to prevent spamming, you're only allowed to ask 2 questions per hour.
+          Please note, to prevent spamming, you are only allowed to ask 2 questions per hour.
         </span>
       </div>
     </form>
