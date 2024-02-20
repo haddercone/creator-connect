3927c6400469fd2096ec287891ff4b797e7244a0
Robin Singh <44robin.rs@gmail.com>
2024-02-20 16:25:17 +0530

    header component updated

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,19 @@
 import Link from "next/link";
 import { useServerSession } from "@/hooks";
+import Image from "next/image";
+
 const Header = async () => {
   const session = await useServerSession();
   return (
     <header className="flex justify-between items-center px-4 py-2 border-b-[1px] border-b-slate-600">
-      <Link href="/" className="text-2xl leading-6 ">
-        Creator Connect
+      <Link href="/" className="leading-6 flex items-center gap-2">
+        <Image src={"/hero-icon.svg"} alt="hero-icon" width={20} height={20}/>
+        <span>Creator connect</span>
       </Link>
       {session ? (
-        <div className="py-2">
-          <p>{session.user.name}</p>
+        <div className="py-2 flex items-center gap-4">
+          <Link href={"/dashboard"}>{session.user.name}</Link>
+          <Image className="rounded-full" alt={session.user.name as string} src={session.user.image as string} width={40} height={40} />
         </div>
       ) : (
         <Link
