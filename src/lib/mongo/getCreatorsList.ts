ecf6ee3943c98bacc4cb68576cd123fdff278255
Robin Singh <44robin.rs@gmail.com>
2024-03-30 18:31:26 +0530

    Revert "rm un-necessory files"
    This reverts commit ba4e753211be60dda6aab5071c7f4bf66610c47c.

---
+++ src/lib/mongo/getCreatorsList.ts
@@ -0,0 +1,23 @@
+import prisma from "@/server/db/PrismaClientSingleton";
+
+export async function getCreatorsList() {
+    try {
+        const response = await prisma.user.findMany({
+            orderBy: {
+                id: "desc"
+            },
+            select: {
+                name: true,
+                username: true,
+                profilePic: true,
+                id: true
+            },
+        })
+        return response;
+    } catch (error) {
+        console.log("Error getting creators list: ", error);
+        return {
+            error: "Error getting creators list"
+        }
+    }
+}
