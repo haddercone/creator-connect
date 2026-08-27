4598bb854d0d5ba19815b8f1e491a1a73aa74c9c
Robin Singh <44robin.rs@gmail.com>
2026-08-27 22:58:54 +0530

    query optimisations

--- src/lib/mongo/geCreatorsByPage.ts
+++ src/lib/mongo/geCreatorsByPage.ts
@@ -4,20 +4,22 @@ import prisma from "@/server/db/PrismaClientSingleton";
 export async function getCreatorsByPage(pageNumber: number, perpage: number) {
   const skip = (pageNumber - 1) * perpage;
   try {
-    const totalUsers = await prisma.user.count();
-    const response = await prisma.user.findMany({
-      orderBy: {
-        id: "desc",
-      },
-      skip: skip,
-      take: perpage,
-      select: {
-        name: true,
-        username: true,
-        profilePic: true,
-        id: true,
-      },
-    });
+    const [totalUsers, response] = await Promise.all([
+      prisma.user.count(),
+      prisma.user.findMany({
+        orderBy: {
+          id: "desc",
+        },
+        skip,
+        take: perpage,
+        select: {
+          name: true,
+          username: true,
+          profilePic: true,
+          id: true,
+        },
+      }),
+    ]);
 
     return { response, totalUsers };
   } catch (error) {
